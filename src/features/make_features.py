c962a09e4474e226a719fd579df6160b51716a3f
Dawson <dawsoneliasen@gmail.com>
2019-04-21 17:21:21 -0600

    add model notebook, implement feature engineering

--- src/features/make_features.py
+++ src/features/make_features.py
@@ -1,49 +1,78 @@
+import datetime as dt
 import numpy as np
 import pandas as pd
+import pdb
 
 def make_predictors(df, date, hood):
     """ Create predictor variables.
-    TODO: this function should resolve issue #27
-    https://github.com/the4thparadigm/denvercrime/issues/27
-    TODO: write a docstring explaining your approach
-    TODO: submit a PR with your changes to this file
 
     Parameters:
         df (DataFrame): crime data
-        date (datetime.date): value for df['REPORTED_DATE']
+        dates (datetime.date): datetime.date obj; date to predict
         hood (str): value for df['NEIGHBORHOOD_ID']
 
     Returns:
-        (Series): Counts of different combinations of date, offense type, and location
-
+        (DataFrame): Rows are indexed by predictions, columns are features (counts for different intervals)
     """
-    return None
+    sub = df[
+        (df['REPORTED_DATE'] < date) & 
+        (df['NEIGHBORHOOD_ID'] == hood)
+    ]
 
-def make_responses(df, date, hood):
+    day = sub[
+        sub['REPORTED_DATE'] > (date - dt.timedelta(days=1))
+    ].groupby('OFFENSE_CATEGORY_ID').count()['INCIDENT_ID']
+
+    week = sub[
+        sub['REPORTED_DATE'] > (date - dt.timedelta(days=7))
+    ].groupby('OFFENSE_CATEGORY_ID').count()['INCIDENT_ID']
+
+    month = sub[
+        sub['REPORTED_DATE'] > (date - dt.timedelta(days=30))
+    ].groupby('OFFENSE_CATEGORY_ID').count()['INCIDENT_ID']
+
+    year = sub[
+        sub['REPORTED_DATE'] > (date - dt.timedelta(days=365))
+    ].groupby('OFFENSE_CATEGORY_ID').count()['INCIDENT_ID']
+
+    return pd.concat(
+        [day,week,month,year],
+        keys=['day', 'week', 'month', 'year'],
+        axis=1,
+        sort=False
+    ).fillna(0)
+
+def make_responses(df, dates, hood, category):
     """ Create response variables.
-    TODO: this function should resolve issue #28
-    https://github.com/the4thparadigm/denvercrime/issues/28
-    TODO: write a docstring explaining your approach
-    TODO: submit a PR with your changes to this file
 
     Parameters:
         df (DataFrame): crime data
-        date (datetime.date): value for df['REPORTED_DATE']
+        dates (list): list of datetime.date objs
         hood (str): value for df['NEIGHBORHOOD_ID']
+        category (str): value for df['OFFENSE_CATEGORY_ID']
 
     Returns:
         (Series): Counts of each offense category for this date and neighborhood
     """
-    return None
+    sub = df[
+        (df['NEIGHBORHOOD_ID'] == hood) &
+        (df['REPORTED_DATE'].isin(dates)) &
+        (df['OFFENSE_TYPE_ID'] == type)
+    ]
+    return sub.groupby('REPORTED_DATE').count()
 
 def main():
     """ Entry point.
-    Calls predictors() and responses() and saves the
-    result in data/processed/features.csv
+    For testing purposes only.
     """
-    crime = pd.read_csv('data/raw/crime.csv')
-    # make_predictors(crime).to_csv('data/processed/predictors.csv', index=False)
-    # make_responses(crime).to_csv('data/processed/responses.csv', index=False)
+    crime = pd.read_csv('data/raw/crime.csv', parse_dates=True)
+    crime['REPORTED_DATE'] = pd.to_datetime(crime['REPORTED_DATE']).dt.normalize()
+    test = make_predictors(
+        crime,
+        dt.datetime.strptime('2018-04-22', '%Y-%m-%d'),
+        'stapleton'
+    )
+    print(test)
 
 if __name__ == '__main__':
     main()
