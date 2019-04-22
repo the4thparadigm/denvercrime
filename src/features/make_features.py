5efaaa30ef3afd1aff8de5ba8f4a746ab5a593ca
fouticus <fout.alex@gmail.com>
2019-04-21 23:18:06 -0600

    added features, fixed dataframe references, added argument to filter make_predictors by category, changed offense type to offense category

--- src/features/make_features.py
+++ src/features/make_features.py
@@ -3,41 +3,103 @@ import numpy as np
 import pandas as pd
 import pdb
 
-def make_predictors(df, date, hood):
+def make_predictors(df, date, hoodname, category):
     """ Create predictor variables.
 
     Parameters:
         df (DataFrame): crime data
         dates (datetime.date): datetime.date obj; date to predict
-        hood (str): value for df['NEIGHBORHOOD_ID']
+        hoodname (str): value for df['NEIGHBORHOOD_ID']
 
     Returns:
         (DataFrame): Rows are indexed by predictions, columns are features (counts for different intervals)
     """
-    sub = df[
+    hood = df[
         (df['REPORTED_DATE'] < date) & 
-        (df['NEIGHBORHOOD_ID'] == hood)
+        (df['NEIGHBORHOOD_ID'] == hoodname) & 
+        (df['OFFENSE_CATEGORY_ID'] == category)
+    ]
+    nonhood = df[
+        (df['REPORTED_DATE'] < date) & 
+        (df['NEIGHBORHOOD_ID'] != hoodname) & 
+        (df['OFFENSE_CATEGORY_ID'] == category)
     ]
 
-    day = sub[
-        sub['REPORTED_DATE'] > (date - dt.timedelta(days=1))
+    yesterday_hood = hood[
+        hood['REPORTED_DATE'] > (date - dt.timedelta(days=1))
+    ].groupby('OFFENSE_CATEGORY_ID').count()['INCIDENT_ID']
+    
+    last3days_hood = hood[
+        hood['REPORTED_DATE'] > (date - dt.timedelta(days=3))
     ].groupby('OFFENSE_CATEGORY_ID').count()['INCIDENT_ID']
 
-    week = sub[
-        sub['REPORTED_DATE'] > (date - dt.timedelta(days=7))
+    lastweek_hood = hood[
+        hood['REPORTED_DATE'] > (date - dt.timedelta(days=7))
     ].groupby('OFFENSE_CATEGORY_ID').count()['INCIDENT_ID']
 
-    month = sub[
-        sub['REPORTED_DATE'] > (date - dt.timedelta(days=30))
+    lastmonth_hood = hood[
+        hood['REPORTED_DATE'] > (date - dt.timedelta(days=30))
     ].groupby('OFFENSE_CATEGORY_ID').count()['INCIDENT_ID']
 
-    year = sub[
-        sub['REPORTED_DATE'] > (date - dt.timedelta(days=365))
+    lastyear_hood = hood[
+        hood['REPORTED_DATE'] > (date - dt.timedelta(days=365))
     ].groupby('OFFENSE_CATEGORY_ID').count()['INCIDENT_ID']
+    
+    weekago_hood = hood[
+        hood['REPORTED_DATE'] == (date - dt.timedelta(days=7))
+    ].groupby('OFFENSE_CATEGORY_ID').count()['INCIDENT_ID']
+    
+    yesterday_nonhood = nonhood[
+        nonhood['REPORTED_DATE'] > (date - dt.timedelta(days=1))
+    ].groupby('OFFENSE_CATEGORY_ID').count()['INCIDENT_ID']
+    
+    last3days_nonhood = nonhood[
+        nonhood['REPORTED_DATE'] > (date - dt.timedelta(days=3))
+    ].groupby('OFFENSE_CATEGORY_ID').count()['INCIDENT_ID']
+
+    lastweek_nonhood = nonhood[
+        nonhood['REPORTED_DATE'] > (date - dt.timedelta(days=7))
+    ].groupby('OFFENSE_CATEGORY_ID').count()['INCIDENT_ID']
+
+    lastmonth_nonhood = nonhood[
+        nonhood['REPORTED_DATE'] > (date - dt.timedelta(days=30))
+    ].groupby('OFFENSE_CATEGORY_ID').count()['INCIDENT_ID']
+
+    lastyear_nonhood = nonhood[
+        nonhood['REPORTED_DATE'] > (date - dt.timedelta(days=365))
+    ].groupby('OFFENSE_CATEGORY_ID').count()['INCIDENT_ID']
+    
+    weekago_nonhood = nonhood[
+        nonhood['REPORTED_DATE'] == (date - dt.timedelta(days=7))
+    ].groupby('OFFENSE_CATEGORY_ID').count()['INCIDENT_ID']
+    
+    #months = [0] * 12
+    #months[date.month-1] = 1
+    #weekdays = [0] * 7
+    #weekdays[date.weekday()] = 1
+    #mwd = pd.DataFrame(data=[months+weekdays], index=[category])
+    mwd = pd.Series(months+weekdays, index=[category]*19)
+    print(type(mwd))
+
+    import pdb; pdb.set_trace()
 
     return pd.concat(
-        [day,week,month,year],
-        keys=['day', 'week', 'month', 'year'],
+        [yesterday_hood, last3days_hood, 
+        lastweek_hood, lastmonth_hood, 
+        lastyear_hood, weekago_hood, 
+        yesterday_nonhood, last3days_nonhood, 
+        lastweek_nonhood, lastmonth_nonhood, 
+        lastyear_nonhood, weekago_nonhood, 
+        keys=['yesterday_hood', 'last3days_hood', 
+              'lastweek_hood', 'lastmonth_hood',
+              'lastyear_hood', 'weekago_hood',
+              'yesterday_nonhood', 'last3days_nonhood', 
+              'lastweek_nonhood', 'lastmonth_nonhood', 
+              'lastyear_nonhood', 'weekago_nonhood'],
+              #'Jan', 'Feb'], 
+              #'Mar', 'Apr', 'May', 'Jun', 'Jul', 
+              #'Aug', 'Sep', 'Oct', 'Nov', 'Dec', 'Mon', 'Tue', 
+              #'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
         axis=1,
         sort=False
     ).fillna(0)
@@ -57,7 +119,7 @@ def make_responses(df, dates, hood, category):
     sub = df[
         (df['NEIGHBORHOOD_ID'] == hood) &
         (df['REPORTED_DATE'].isin(dates)) &
-        (df['OFFENSE_TYPE_ID'] == type)
+        (df['OFFENSE_CATEGORY_ID'] == category)
     ]
     return sub.groupby('REPORTED_DATE').count()
 
@@ -67,12 +129,16 @@ def main():
     """
     crime = pd.read_csv('data/raw/crime.csv', parse_dates=True)
     crime['REPORTED_DATE'] = pd.to_datetime(crime['REPORTED_DATE']).dt.normalize()
+    crime.to_pickle('data/processed/crime.pkl')
+    crime = pd.read_pickle('data/processed/crime.pkl')
     test = make_predictors(
         crime,
         dt.datetime.strptime('2018-04-22', '%Y-%m-%d'),
-        'stapleton'
+        'stapleton',
+        'burglary'
     )
-    print(test)
+    print(test.T)
+    import pdb; pdb.set_trace()
 
 if __name__ == '__main__':
     main()
